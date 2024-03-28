a382824a2588bc07f2d5af8743a41ce7152f4052
Swati Sri Thingbaijam <thingbaijamswatisri@gmail.com>
2024-03-28 17:26:07 +0530

    netflix sample

---
+++ script.js
@@ -0,0 +1,238 @@
+let movies =[
+    {  
+    Name : "Descendants of the Sun",
+    Poster:"https://imgs.search.brave.com/KkiErFm2dZfPQZCF3OWjRcCPn7iN_wgGzrGTX3uScoE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL00v/TVY1Qk0yRXhOR0U0/WmpJdE1tWTVNeTAw/WkRVNUxXSmlNek10/T1RGak1EUm1OV1Zs/TXpaaVhrRXlYa0Zx/Y0dkZVFYVnlNekU0/TURreU5UQUAuanBn",
+    Cast:"Song Joong-Ki,Song Hye-Kyo, Jin Goo, Kim Ji-Won",
+    Rating: 8.7,
+    
+
+    },
+
+    {  
+        Name :"Vincenzo",
+        Poster:"https://imgs.search.brave.com/D6Fr7rB4jU7nlQLNAV0eVbMByu6YztCBF3lcxMohdLc/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL00v/TVY1QllXSXdZVGcy/WTJZdFl6RXdOeTAw/WTJZMExXRmtaamt0/TmpCbE1XWmhORFky/TkdFMFhrRXlYa0Zx/Y0dkZVFYVnlORFk1/TWpNeU5UZ0AuanBn",
+        Cast:"Song Joong-Ki, Jeon Yeo-Been,Ok Taec-yeon,Kim Yeo Jin,Kwak Dong Yeon",
+        Rating: 9.1
+    },
+
+   {  
+            Name :"Hotel Deluna",
+            Poster:"https://imgs.search.brave.com/1KfwVR7GMFVGrnCmaxQOuV3JUYntVynG11GGOpH_4uM/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NjFib0xhbkhKa0wu/anBn",
+            Cast:"IU,Yeo Jin-goo,Lee Joon-gi,Kang Mi-na,P.O ",
+            Rating: 9.2,
+            
+   },
+
+   {  
+    Name :"Alchemy Of Souls-1",
+    Poster:"https://imgs.search.brave.com/LyqC9qUtnSK43myj_AoAg5pfantDp4osJLssyu0SBaU/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMubmV0bmFpamEu/Y29tL2kvenhHN1pt/UUVLcTIuanBn",
+    Cast:"Lee Jae Wook,Jung So-Min,Hwang Min-Hyun,Shin Seung-Ho,Yoo In-Soo,Arin ", 
+    Rating: 8.4
+    
+    },
+
+  {  
+    Name :"Alchemy Of Souls-2",
+    Poster:"https://imgs.search.brave.com/aXX0AmHANYwR4wHc9J_UiRqaU3BTQN502airuCwstIA/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pLm15/ZHJhbWFsaXN0LmNv/bS9YMDZSZF80Yy5q/cGc_dj0x",
+    Cast:"Lee Jae Wook,Go Youn-Jung,Hwang Min-Hyun,Shin Seung-Ho,Yoo In-Soo,Arin",
+    Rating: 8.3
+  },
+  {  
+    Name :"Nevertheless",
+    Poster:"https://imgs.search.brave.com/pdwoXcpkjdsa1s6EecTBr-neBoAd7Ih3ltlaDStNZKw/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NjFaM3lFZFE4SUwu/anBn",
+    Cast:"Song Kang,Han So-hee,Chae Jong Hyeop",
+    Rating: 8.1
+},
+{  
+    Name :"Goblin",
+    Poster:"https://imgs.search.brave.com/-1AlFMnNi1HANEEquia5lOk7DCj6CwjWBOkHUnCcsV8/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pLm15/ZHJhbWFsaXN0LmNv/bS9Ka0Q4WGMuanBn/P3Y9MQ",
+    Cast:"Gong Yoo, Kim Do-Eun,Lee Dong Wook,Yoo In Na,Yook Sung Jae ",
+    Rating: 9.1
+},
+{  
+    Name :"My Demon",
+    Poster:"https://imgs.search.brave.com/6bFssPa9sLL-3GhI6YTAF-qil1wnCRenNwXrOnCg3A4/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL00v/TVY1QllqUTBOV1l3/TW1ZdFlUQTVPQzAw/T0RRM0xUa3dPREl0/WkRVM05qWTFZak0x/WlRsaVhrRXlYa0Zx/Y0dkZVFYVnlOakk0/TkRZNU9ETUAuanBn",
+    Cast:"Song Kang,KIim Yoo-Jung,Lee Sang-Yi",
+    Rating: 9.2
+},
+{  
+    Name :"Doom At Your Service",
+    Poster:"https://imgs.search.brave.com/XJp0Pc80uxxGt1h3d_5zcNN-TqczFXTSDPay4AG9nHQ/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWcu/a3BvcG1hcC5jb20v/NzgweDAvMjAyMS8w/NC9Eb29tLWF0LXlv/dXItc2VydmljZS10/dm4tcG9zdGVyLTIu/anBn",
+    Cast:"Seo In-Guk,Park Bo-Young,Lee Soo-hyuk,Shin Do-hyun",
+    Rating: 8.4
+},
+{  
+    Name :"W-2 Worlds Apart",
+    Poster:"https://imgs.search.brave.com/0iZt4JujWLx1NLCPYPAU2_CBhouGiKPJprexxBp5ECA/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS50aGVtb3ZpZWRi/Lm9yZy90L3AvdzMw/MF9hbmRfaDQ1MF9i/ZXN0djIvckJ1Q29T/MFZCOU02emtFVUN2/blZyZmVGMEouanBn",
+    Cast:"Lee Jong-Suk,Han Hyo-joo",
+    Rating: 7.5
+},
+{  
+    Name :"VAGABOND",
+    Poster:"https://imgs.search.brave.com/yms0_U9IB93W_VNVDHwmrQgdZThAI69ZSHlrqGUST_U/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/dGVhaHViLmlvL3Bo/b3Rvcy9mdWxsLzIw/OC0yMDg3NjQ3X3Zh/Z2Fib25kLWtvcmVh/bi1kcmFtYS1wb3N0/ZXIuanBn",
+    Cast:"Bae Suzy,Lee Seung-Gi",
+    Rating: 7.5
+},
+{  
+    Name :"ABYSS",
+    Poster:"https://imgs.search.brave.com/-nFRSmIAHktEoq07dh3xeqxmInAJdrJfk_60nxfhlHM/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9hc2lh/bndpa2kuY29tL2lt/YWdlcy81LzVhL0Fi/eXNzXyhLb3JlYW5f/RHJhbWEpLXAwMS5q/cGc",
+    Cast:"Ahn Hyo-seop,Park Bo-young",
+    Rating: 7.6
+},
+{  
+    Name :"Destined With You",
+    Poster:"https://imgs.search.brave.com/aao-NhBG-wkqFuQVG8lqfcX08rv2hEeLkkX77y_8HQU/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pLm15/ZHJhbWFsaXN0LmNv/bS9lWThRMFlfNGMu/anBnP3Y9MQ",
+    Cast:"Rowoon,Jo Bo-ah",
+    Rating: 8.9
+},
+{
+  Name:"While You Were Sleeping",
+  Poster:"https://imgs.search.brave.com/GT9JfZS66wQdpQGT-nChA4vXRTFKEzK6EAAe7yzsFuA/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pLm15/ZHJhbWFsaXN0LmNv/bS9qUVY0NUpfNGMu/anBnP3Y9MQ",
+  Cast:"Lee Jong-suk,Bae Suzy,Jung Hae-In",
+  Rating:8.9
+},
+{
+  Name:"Doctor Slump",
+  Poster:"https://imgs.search.brave.com/PCf0QHQdeW5m4ahL9OGmA4j78p5jkaYnkucqsnIQ1qk/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS50aGVtb3ZpZWRi/Lm9yZy90L3AvdzMw/MF9hbmRfaDQ1MF9i/ZXN0djIvaU9XbWJa/RWJodnJZeVdtNk80/VzNvSGYyUzlCLmpw/Zw",
+  Cast:"Park Hyung-Sik,Park Shin-Hye,Yoon Park,Gong Seong-Ha",
+  Rating:8.6
+},
+     
+{
+  Name:"King The Land",
+  Poster:"https://imgs.search.brave.com/BBkfeAQJG0isYkVzPt0Nus00lh2ZcP3WtIYNoX0s9ew/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9hc2lh/bndpa2kuY29tL2lt/YWdlcy83LzdmL0tp/bmdfdGhlX0xhbmQt/cDEuanBn",
+  Cast:"Lee Jun-Ho,Im Yoon-Ah",
+  Rating:9.2
+},
+{
+  Name:"True Beauty",
+  Poster:"https://imgs.search.brave.com/Yn6pVRynKZ_BjDGG42oR1NcObVZFsZWQ_9Nk2W3zhoY/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pLm15/ZHJhbWFsaXN0LmNv/bS9xUDJrS180Yy5q/cGc_dj0x",
+  Cast:"Cha Eun-woo,Moon Ga-young,Hwang In-youp,Park Yoo-na",
+  Rating:8.7
+},
+{
+  Name:"My ID Is GangNam Beauty",
+  Poster:"https://imgs.search.brave.com/XJHX4RB1meINi_voOHjcXUnLhFt1jwSZ-EvePugSeh8/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9hc2lh/bndpa2kuY29tL2lt/YWdlcy81LzViL015/X0lEX0lzX0dhbmdu/YW1fQmVhdXR5LVAx/LmpwZw",
+  Cast:"Cha Eun-woo,Jo Woo-ri,Kwak Dong-yeon",
+  Rating:8.5
+},
+{
+  Name:"Business Proposal",
+  Poster:"https://imgs.search.brave.com/vebGCrvNS9C_yuBxMeR-vGuXx_OzBMNxmaP6sslEsvE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc3VtbWl0bWVk/aWEtZGlnaXRhbC5j/b20vcHJldmlldy9p/bWFnZXMvMjAyMi8w/My8xMS9QSE9UTy1C/WS1JTlNUQUdSQU1f/c2JzZHJhbWEub2Zm/aWNpYWwuMS5qcGc",
+  Cast:"Ahn Hyo-seop,Kim Se-jeong,Kim Min-kyu,Seol In-ah",
+  Rating:8.8
+},
+{
+  Name:"Queen of Tears",
+  Poster:"https://imgs.search.brave.com/HBzZefy3YF0NnXStFa1Ji38UFBmXtZr_zJ803ZJsOVY/rs:fit:500:0:0/g:ce/aHR0cHM6Ly8wLnNv/b21waS5pby93cC1j/b250ZW50L3VwbG9h/ZHMvMjAyNC8wMi8y/NTE4MDAxOC9xdWVl/bi1vZi10ZWFycy0y/LmpwZWc",
+  Cast:"Kim Soo-Hyun,Kim Ji-Won",
+  Rating:8.9
+},
+{
+  Name:"Twinkling Watermelon",
+  Poster:"https://imgs.search.brave.com/A2Ud4BvrGuQ9_XpvNcY3AFvL7a4ZJtZLdcZuQPLKTv0/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWcu/d2VncmVlbmt3LmNv/bS9zaXRlcy8zLzIw/MjMvMDgvVHdpbmts/aW5nLVdhdGVybWVs/b24tMjAyMy53ZWJw",
+  Cast:"Ryeoun,Choi Hyun-wook,Seol In-ah,Shin Eun-Soo",
+  Rating:9.2
+},
+{
+  Name:"It's Okay To Not Be Okay",
+  Poster:"https://imgs.search.brave.com/DgFimMj2eCyI62EzRd3gxqR0XrubSAHq09lFIwHGkSM/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pLm15/ZHJhbWFsaXN0LmNv/bS94dzN4WF80Yy5q/cGc_dj0x",
+  Cast:"Kim Soo-Hyun,Seo Yea-ji,Park Gyu-young",
+  Rating:7.9
+},
+{
+  Name:"Shooting Stars",
+  Poster:"https://imgs.search.brave.com/Q0TTVJA-YR8v8fiX5f3mLkBrkH5t2iwS7frbPbIv93w/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMtbmEuc3NsLWlt/YWdlcy1hbWF6b24u/Y29tL2ltYWdlcy9J/LzUxOXcyWWNOVWhM/LmpwZw",
+  Cast:"Lee Sung-kyung,Kim Young-dae,Yoon Jong-hoon",
+  Rating:7.8
+},
+{
+  Name:"Start-Up",
+  Poster:"https://imgs.search.brave.com/f6C2sMZd8UWqU3-CwDdVnhEB8rR16Pzy9u7zzvAmPHQ/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pLm15/ZHJhbWFsaXN0LmNv/bS9SUUxlWV80Yy5q/cGc_dj0x",
+  Cast:"Nam Joo-hyuk,Bae Suzy,Kim Seon-ho,Kang Han-na",
+  Rating:8.1
+},
+     
+     
+     
+     
+]; 
+
+
+function searchMovie(){
+   let movieName = document.getElementById('search').value; 
+   
+   if(movieName!=="")
+   {
+     let result = movies.filter(function(movie)
+      {
+        return movie.Name.toUpperCase().includes (movieName.toUpperCase())
+      })
+
+     displayMovies(result);
+
+   }
+   else
+   {
+
+    displayMovies(movies);
+   }
+  
+}
+
+function displayMovies(data)
+{
+  document.getElementById("movies").innerHTML="";
+
+
+   let htmlString =` `;
+
+   for(let i=0;i<data.length;i++)
+   {
+    htmlString=htmlString+`
+    <div class="movie">
+   <div class="overlay">
+    //  <div class="video">
+
+    //  </div>
+     <div class="details">
+       <h1>${data[i].Name}</h1>
+       <h2>IMDB : ${data[i].Rating}</h2>
+       <p>Cast : ${data[i].Cast}</p>
+     </div>
+   </div>
+   <img class="poster" src="${data[i].Poster}">
+   
+ </div>
+    
+    
+    `;
+
+   }
+console.log(htmlString);
+
+ 
+document.getElementById("movies").innerHTML=htmlString;
+   
+   
+   
+   
+}
+
+displayMovies(movies);
+
+
+
+
+
+ // let movieDIV = document.createElement("div");
+    // movieDIV.classList.add("movie");
+    
+
+    // let overlayDIV = document.createElement("div");
+    // overlayDIV.classList.add("overlay");
+
+    // movieDIV.appendChild(overlayDIV);
+
+    // console.log(movieDIV); 
+    
